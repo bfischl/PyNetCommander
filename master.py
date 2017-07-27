142649d11eaa511de17f2d726acaa8c6d8d71b0c
Brad Fischl <bradley.fischl@gmail.com>
2017-07-27 09:37:23 -0400

    Modified to make more Python3 friendly

--- master.py
+++ master.py
@@ -7,11 +7,13 @@ from flask import Flask, jsonify, make_response, request, abort
 
 app = Flask(__name__)
 
-class sheep:
-    def __init__(self, id, ip, mac):
+
+class minion:
+    def __init__(self, id, ip, mac, platform):
         self.sheep_id = id
         self.ip = ip
-        self.mac= mac
+        self.mac = mac
+        self.platform = platform
         self.hashcode = None
 
     def gethash(self):
@@ -32,14 +34,14 @@ def get_next_id():
 
 
 def load_settings(filename):
-    with open(filename, 'rb') as f:
+    with open(filename, encoding='utf-8') as f:
         tmp_settings = json.load(f)
     return tmp_settings
 
 def load_tasks():
     tasks = [{
-        'task_id': 1,
-        'sheep_id': 1
+        "task_id": 1,
+        "sheep_id": 1
     }]
     return tasks
 
@@ -49,21 +51,20 @@ def not_found(error):
     return make_response(jsonify({'error':'Not found'}),404)
 
 
-@app.route('/sheep/1/tasks/<str:sheep_id>', methods=['GET'])
+@app.route('/minion/1/tasks/<int:sheep_id>', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({"tasks": tasks})
 
 
-@app.route('/sheep/1/register', methods=['POST'])
+@app.route('/minion/1/register', methods=['POST'])
 def register_new():
     if not request.json:
         abort(400)
-    tmp_sheep = sheep(get_next_id(), request.remote_addr,request.json["mac"])
-
-    if add_minion(tmp_sheep) != 0:
+    tmp_minion = minion(get_next_id(), request.remote_addr, request.json["mac"], request.json["platform"])
+    if add_minion(tmp_minion) != 0:
         return abort(500)
     else:
-        return jsonify({'hashcode':tmp_sheep.gethash}), 201
+        return jsonify({"hashcode":tmp_minion.gethash()}), 201
 
 
 if __name__ == '__main__':
--- minion.py
+++ minion.py
@@ -1,34 +1,52 @@
-import json, os, socket, fcntl, struct, platform, requests
+#!/usr/bin/env python3
+import json
+import platform
+import requests
 from uuid import getnode
 import minion_lib
 
+
 def load_settings(filename):
-    with open(filename,'rb') as f:
+    """
+    :param filename:
+    :return:
+    """
+    with open(filename, encoding='utf-8') as f:
         tmp_settings = json.load(f)
     return tmp_settings
+
 
 def get_mac():
     return hex(getnode())
 
-class sheep:
+class minion:
     def __init__(self, mac, platform, settings):
         self.mac = mac
         self.platform = platform
         self.settings = settings
         self.hashcode = None
         self.available_commands = []
+
     def register(self):
+        """ Connects to Master and registers, receives hashcode
+        :return: 0 on success
+        """
         if settings['https'] == 1:
             url = "https://"
         else:
             url = "http://"
         url += settings['master_ip'] + ':' + str(settings['master_port']) + settings['register']
         print(url)
-        payload = {"platform": self.platform, "mac": self.mac, "available": self.available_commands}
-        r = requests.post(url, json=payload)
-        if r.status_code == 201:
-            self.hashcode = r.json()['hashcode']
-            print('HASHCODE:\t%s', self.hashcode)
+        payload = {"platform": self.platform, "mac": str(self.mac), "available": str(self.available_commands)}
+        try:
+            r = requests.post(url, json=payload)
+            if r.status_code == 201:
+                self.hashcode = r.json()['hashcode']
+                print('HASHCODE:\t%s' % self.hashcode)
+        except requests.ConnectionError as e:
+            print("FAIL TO CONNECT")
+
+
 
     def get_tasks(self):
         # Implement retrieve tasks from master
@@ -41,6 +59,6 @@ class sheep:
 
 if __name__ == "__main__" :
     settings = load_settings('minion_settings.json')
-    me = sheep(get_mac(), platform.platform(), settings)
+    me = minion(get_mac(), platform.platform(), settings)
     me.available_commands.append(a for a in dir(minion_lib) if not a.startswith('__'))
     me.register()
